c80e87001255e0ea8411a2d4842db5c2201f4f80
Haziq Hakimi <haziqhakimiyes@gmail.com>
2025-01-08 17:16:04 +0800

    updated with working form response

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+
+# Constant
+MY_EMAIL = "sweetsugarpastry@gmail.com"
+MY_PASSWORD = "rqaarmydmibkgzdl" # Gmail App password
+TARGET_EMAIL = "haziqhakimiyes@gmail.com"
 
 # API
 API_ENDPOINT = "https://api.npoint.io/0a998b24f76ffdde88d1"
@@ -10,6 +16,13 @@ response_data = response.json()
 
 # Create an instance of flask class
 app = Flask(__name__)
+
+def send_email(name, email, number, message):
+	email_message = f"Subject:Blog Feedback\n\nName: {name}\nEmail: {email}\nPhone Number: {number}\nMessage: {message}"
+	with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+		connection.starttls()
+		connection.login(MY_EMAIL, MY_PASSWORD)
+		connection.sendmail(MY_EMAIL, TARGET_EMAIL, email_message)
 
 # Homepage
 @app.route("/")
@@ -32,6 +45,20 @@ def post(index):
 		if blog["id"] == index:
 			requested_post = blog
 	return render_template("post.html", post=requested_post, filename=filename)
+
+@app.route("/contact", methods=["POST"])
+def receive_form():
+	if request.method == 'GET':
+		return render_template("contact.html", msg_sent=False)
+	else:
+		# name = request.form["username"]
+		# email = request.form["email"]
+		# number = request.form["number"]
+		# message = request.form["message"]
+		# print(f"Name: {name}\nEmail: {email}\nPhone Number: {number}\nMessage: {message}")
+		data = request.form
+		send_email(data["username"], data["email"], data["number"], data["message"])
+		return render_template("contact.html", msg_sent=True)
 
 # Run app
 if __name__ == "__main__":
